04ea44e866653590b9b043746206e31619cd5c05
bharathvenkatesan467 <bharathvenkatesan100gmail.com>
2024-08-27 17:08:06 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const initialstate = [
+  ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+  ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const chesscoins = {
+  R: '\u2656',
+  N: '\u2658',
+  B: '\u2657',
+  Q: '\u2655',
+  K: '\u2654',
+  P: '\u2659',
+  r: '\u265C',
+  n: '\u265E',
+  b: '\u265D',
+  q: '\u265B',
+  k: '\u265A',
+  p: '\u265F',
+};
+
+const ChessBoard = () => {
+  const [board, setBoard] = useState(initialstate);
+
+  const renderSquare = (piece, row, col) => {
+    const White = (row + col) % 2 === 0;
+    const chesscoin = chesscoins[piece];
+
+    return (
+      <div key={`${row}-${col}`} className={`square ${White ? 'white' : 'black'}`}> {chesscoin}</div>
+    );
+  };
+
+  return (
+    <div class='b1'>
+      <h2>Chess Board</h2>
+      <div className="chessboard">
+      {board.map((row, rowIndex) =>
+        row.map((piece, colIndex) =>
+          renderSquare(piece, rowIndex, colIndex)
+        )
+      )}
+    </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<ChessBoard/>,document.getElementById('root'));
